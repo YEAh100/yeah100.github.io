6ee0e7f7deb9dadf17f3bb68cb5746af54d0dffc
YEAh100 <168049547+YEAh100@users.noreply.github.com>
2024-04-26 23:22:05 +0000

    update 1.00

---
+++ yeah.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+import requests
+import spacy
+
+app = Flask(__name__)
+nlp = spacy.load("en_core_web_sm")
+
+@app.route('/scrape_and_extract', methods=['POST'])
+def scrape_and_extract():
+    url = request.json['url']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    text = ' '.join(soup.stripped_strings)
+    doc = nlp(text)
+    entities = {ent.text: ent.label_ for ent in doc.ents}
+    return jsonify(entities)
+
+if __name__ == '__main__':
+    app.run(port=5000)
